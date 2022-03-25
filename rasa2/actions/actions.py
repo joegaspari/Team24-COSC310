1f7ac72c647bfa43f8becb80dcfc40ad99f88773
joegaspari <77291565+joegaspari@users.noreply.github.com>
2022-03-25 13:48:42 -0700

    Adding static call

--- rasa2/actions/actions.py
+++ rasa2/actions/actions.py
@@ -155,7 +155,7 @@ class ActionSubmitFlightForm1(Action):
        
         url4 = "https://skyscanner44.p.rapidapi.com/search-extended"
         
-        querystring3 = {"adults":"1","origin":"YYZ","destination":"YVR","departureDate":"2022-06-28","returnDate":"2022-07-28","currency":"CAD"}
+        querystring3 = {"adults":"1","origin":"YVR","destination":"YYZ","departureDate":"2022-08-01","returnDate":"2022-08-10","currency":"CAD"}
         print(querystring3)
         headers3 = {
 	        "X-RapidAPI-Host": "skyscanner44.p.rapidapi.com",
@@ -213,9 +213,7 @@ class ActionSubmitFlightForm1(Action):
    
 # one way
 
-from bs4 import BeautifulSoup
-import urllib.request
-import re
+
 class ActionSubmitFlightForm2(Action):
      
      
@@ -237,7 +235,8 @@ class ActionSubmitFlightForm2(Action):
         departC = tracker.get_slot('departureC')
         arrivalC = tracker.get_slot('arrivalC')
         dDate = tracker.get_slot('departure_date')
-       
+    
+    
         # Get airport code from city slot name
         depart_code = 'Not found'
         arrival_code = 'Not found'
@@ -247,30 +246,62 @@ class ActionSubmitFlightForm2(Action):
                 arrival_code = e['code']
             if(e['city'] == departC):
                 depart_code = e['code']
-                
-
-        
-        #Now that you have the airport code saved int depart_code and arrival_code we can use the skypicker
-        # api to aquire the flight information
-        
-        quote_page = "https://flights.makemytrip.com/makemytrip/search/O/O/E/1/0/0/S/V0/{}_{}_{}?contains=false&remove="
-        page=urllib.request.urlopen(quote_page.format(depart_code,arrival_code,dDate))
-        soup = BeautifulSoup(page, 'html.parser')
-        list1=[]
-        message=soup.find_all('label',attrs={'class':'flL mtop5 mleft3 vallabel'})
-        dispatcher.utter_message("Here is the list of carriers with their fare")
-        for a in message:
-            list1.append(a.text)
-        message1=soup.find_all('span',attrs={'class':'flR'})
-        list2=[]
-        for b in message1:
-            if "Rs." in b.text:
-                list2.append(re.sub('\s+', '', b.text))
-        for i in range(len(list1)):
-            dispatcher.utter_message(list1[i]+" : "+list2[i])
-        return []
-       
-      
+            
+        f1.close()
     
+    #Now that you have the airport code saved int depart_code and arrival_code we can use the skypicker
+    # api to aquire the flight information
+    
+        print(f"depart_code = {depart_code}, arrival_code = {arrival_code}, depart city = {departC}, arrival city = {arrivalC}, departureDate = {dDate}")
+    
+        url = "https://skyscanner44.p.rapidapi.com/search-extended"
+    
+        querystring = {"adults":"1","origin":"YVR","destination":"LHR","departureDate":"2022-09-23","currency":"CAD"}
+        print(querystring)
+        headers = {
+        "X-RapidAPI-Host": "skyscanner44.p.rapidapi.com",
+        "X-RapidAPI-Key": "90a274727dmsh607a63ae7dd7473p12f953jsn5e3fb6071646"
+        }
+
+        response = requests.request("GET", url, headers=headers, params=querystring).json()
+
+
+        string_builder = ''
+        temp_airlines_added = []
+        i = 1
+
+
+        for e in response['itineraries']['results']:
+            price = e['pricing_options'][0]['price']['amount']
+            for ee in e['legs']:
+
+                if(ee['origin']['displayCode'] == 'YYZ' and ee['segments'][0]['operatingCarrier']['name'] not in temp_airlines_added):
+                    temp_airlines_added.append(ee['segments'][0]['operatingCarrier']['name'])
+
+                    if(i == 1):
+                        flight_num = 'Flight #{}'.format(i)
+                    else:
+                        flight_num = '\nFlight #{}'.format(i)
+
+                    flight_with = 'Flying with {}'.format(ee['segments'][0]['operatingCarrier']['name'])
+                    depart_on = 'Departing on {}'.format(ee['departure'][0:10] + ' @ ' + ee['departure'][-8:])
+                    arriv_on = 'Arriving on {}'.format(ee['arrival'][0:10] + ' @ ' + ee['arrival'][-8:])
+                    price_send = 'Which will cost you aboot: ${}'.format(price)
+                
+                    string_builder += flight_num + '\n'
+                    string_builder += flight_with + '\n'
+                    string_builder += depart_on + '\n'
+                    string_builder += arriv_on + '\n'
+                    string_builder += price_send 
+
+                    i += 1
+                if(i >= 10):
+                    break
+    
+    
+        print(string_builder)
+        dispatcher.utter_message(string_builder)
+    
+        return []
     
 
